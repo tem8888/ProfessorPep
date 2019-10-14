42b46d3f90fa327a22d779c0c0f89fc66577ee48
tem <gamegir@gmail.com>
2019-10-15 02:54:34 +0300

    upd

--- clientpool.js
+++ clientpool.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 
 var config = {
         port: 5432,
-        host: "ec2-54-247-171-30.eu-west-1.compute.amazonaws.com",
+        host: process.env.DB_HOST,
         database: process.env.DB_DATABASE,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
