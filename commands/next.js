537f79c0e73e9f3aa3f8a92b698e32ac911a6383
tem <gamegir@gmail.com>
2019-10-29 20:42:25 +0300

    upd

--- commands/next.js
+++ commands/next.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const config = require('../config.js');
-const congrat = require('./congrat.js');
+const congrat = require('../congrat.js');
 
 module.exports.run = async (client, message, randm, line, question, answer) => { 
 	if (!client) {
