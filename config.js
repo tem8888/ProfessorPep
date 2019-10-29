bee18332186e606e9e7fa20423969b9ec0c07e01
tem <gamegir@gmail.com>
2019-10-29 20:28:03 +0300

    upd

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	"idOwner": "330832623435907076",
-  	"botToken"  : "NTA0MDA3MTM2MTcxMzkzMDQ0.XaQ4-w.w7sm4Sx_pLa9VXvu7amKH2NJIjQ",
     "prefix": "!",
     "url_file" : "questions.txt",
     "time_to_hint" : "8500",
