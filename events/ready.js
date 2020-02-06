354630352cadf15a7c6bbf2c5fbd184cd5c51cd5
tem <gamegir@gmail.com>
2020-02-06 13:18:59 +0300

    upd

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (bot) => {
 
 		console.log(`${bot.user.tag} has started in ${bot.guilds.size} guilds.`);
-  		bot.user.setActivity(`!qhelp -- on ${bot.guilds.size} servers`);
+  		bot.user.setActivity(`Викторина !старт`);
 
 };
