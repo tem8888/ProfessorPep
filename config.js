67e24654247510357d87f465b44cffe1cc1075ce
tem <gamegir@gmail.com>
2020-02-06 13:13:03 +0300

    upd

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"idOwner": "330832623435907076",
-  	"botToken"  : "NjMxNjM4NTc4NDM4NjAyNzUy.XjvkMw.OkQ2tVOSsV1sI6PnVDQUk6CsKBM",
+  	"botToken"  : "NjMxNjM4NTc4NDM4NjAyNzUy.XjvmlA.hAHs5GOjAlA67_SM_jU1IN75Zx8",
     "prefix": "!",
     "url_file" : "questions.txt",
     "hintTime" : "8000",
